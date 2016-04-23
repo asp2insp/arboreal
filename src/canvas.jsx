af0d5025f16052e9077448a15c1dc934fb8a07e5
Josiah Gaskin <jcg@fb.com>
2016-04-22 22:08:56 -0700

    Adding separate rand for leaf

--- src/canvas.jsx
+++ src/canvas.jsx
@@ -3,6 +3,13 @@ import MersenneTwister from 'mersenne-twister'
 import { myReactor, getters } from './reactor'
 import { Tree } from './tree'
 
+const createRand = function() {
+  const mt = new MersenneTwister(state.seed);
+  return function(min, max) {
+    return this.random_incl()*(max-min) + min
+  }.bind(mt)
+}
+
 const Canvas = React.createClass({
   mixins: [myReactor.ReactMixin],
 
@@ -15,17 +22,15 @@ const Canvas = React.createClass({
   render() {
     const style = this.props.style;
     const state = this.state.treeParams.toJS()
-    const mt = new MersenneTwister(state.seed);
-    const getRand = function(min, max) {
-      return this.random_incl()*(max-min) + min
-    }.bind(mt)
+
     return (
       <svg style={style} viewBox="0 0 100 100">
         <Tree
           x={50-state.initialWidth/2} y={100}
           length={state.initialLength} strokeWidth={state.initialWidth}
           depth={0} dy={-1} dx={0}
-          rand={getRand} currentAngle={state.initialAngle}
+          rand={createRand()} currentAngle={state.initialAngle}
+          leafRand={createRand()}
         />
       </svg>
     )
